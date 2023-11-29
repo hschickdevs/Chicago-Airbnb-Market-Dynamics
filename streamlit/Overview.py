bfea7cc5ff03cbe26b08e8e028f1374a50f1d2b0
William Dang <71603417+williampdang@users.noreply.github.com>
2023-11-29 15:10:15 -0600

    Add Tools Section
    Added Tools Section
    Add image to data sample

--- streamlit/Overview.py
+++ streamlit/Overview.py
@@ -5,8 +5,8 @@ from util import handle_env
 handle_env()
 
 st.set_page_config(
-    page_title="Overview",
-    page_icon="🏠",
+   page_title="Overview",
+   page_icon="🏠",
 )
 
 st.sidebar.success("☝️ Select an analysis theme above to explore.")
@@ -61,25 +61,62 @@ st.image("https://drive.google.com/uc?export=view&id=1vaXGXW-6aqXb-hlHTQEFQfcaPr
 st.image("https://drive.google.com/uc?export=view&id=13yjSbRyCM4c70JmTnlLkPCGsur-JM5eH")
 st.image("https://drive.google.com/uc?export=view&id=14LE_tnSKHNl5knflnPeV_-TgKHEKNqca")
 st.image("https://drive.google.com/uc?export=view&id=1n6A46KB2hXGSt9AykqHWozDQlXNy5U6q")
+st.image("https://drive.google.com/uc?export=view&id=1zfKZYPrVtE-j7ksvTsxIGbjeM4pJawiI")
 
 st.markdown(
-    """
-    ## Data Cleaning
+   """
+   ## Data Cleaning
 
-    The main ETL tools we used were Python Jupyter notebooks for data extraction and transformation and the Snowflake console wizard for data loading.
+   The main ETL tools we used were Python Jupyter notebooks for data extraction and transformation and the Snowflake console wizard for data loading.
 
-    Our cleaning startegy began with extracting only the relevant columns from the listings.csv (columns that pertained to data collection, unnecessary web links, and columns that lacked any data). Then we cleaned up columns with false but relevant data pertaining to information on bed, bedrooms, and bathrooms counts. We were able to ressed these columns with values that were extracted from another column that contained the listing name which had the actual value counts for these columns. We also encoded columns with categorical variables by creating new columns with numerical values that mapped to those categories; these columns included. The next thing we did to clean this data was fix NaN values in numerical columns by imputing those values with the columnar mean. 
+   Our cleaning startegy began with extracting only the relevant columns from the listings.csv (columns that pertained to data collection, unnecessary web links, and columns that lacked any data). Then we cleaned up columns with false but relevant data pertaining to information on bed, bedrooms, and bathrooms counts. We were able to ressed these columns with values that were extracted from another column that contained the listing name which had the actual value counts for these columns. We also encoded columns with categorical variables by creating new columns with numerical values that mapped to those categories; these columns included. The next thing we did to clean this data was fix NaN values in numerical columns by imputing those values with the columnar mean.
 
-    After working on the listings.csv we moved onto the reviews.csv and calendars.csv of which we removed columns that were not relevant such as reviewer first names since those names did not have a context and could be repeated for different people. Then, since the reviews, calendar, and listings data all contained primary and foreign key values that mapped to each other but were extremely large, we decided to map those values and reset their sequences to start from 1 - this would help us drastically reduce the data size of our tables.
+   After working on the listings.csv we moved onto the reviews.csv and calendars.csv of which we removed columns that were not relevant such as reviewer first names since those names did not have a context and could be repeated for different people. Then, since the reviews, calendar, and listings data all contained primary and foreign key values that mapped to each other but were extremely large, we decided to map those values and reset their sequences to start from 1 - this would help us drastically reduce the data size of our tables.
 
-    The next piece of work was to process the Chicago crime data by mapping a json list of Chicago neighborhoods to the their neighborhood numbers in the crime dataset. We then grouped by the communities in the crime data to compute a relative crime rate to produce a new communities.csv that contained the community number, community name, relative crime rate, and geospatial boundaries. After we mapped community names to community numbers, we went back to the listings.csv to impute a column to hold community numbers that corresponded to the communities where the listings were hosted. 
+   The next piece of work was to process the Chicago crime data by mapping a json list of Chicago neighborhoods to the their neighborhood numbers in the crime dataset. We then grouped by the communities in the crime data to compute a relative crime rate to produce a new communities.csv that contained the community number, community name, relative crime rate, and geospatial boundaries. After we mapped community names to community numbers, we went back to the listings.csv to impute a column to hold community numbers that corresponded to the communities where the listings were hosted.
 
-    The last piece of transformation was to break down the reviews.csv and calendars.csv files into smaller chunks since they exceeded the 50MB loading capacity of the Snowflake Console Wizard.
+   The last piece of transformation was to break down the reviews.csv and calendars.csv files into smaller chunks since they exceeded the 50MB loading capacity of the Snowflake Console Wizard.
 
-    Once those CSVs were broken down, we used the Snowflake Console Wizard to create tables within a database instance and schema inside of Snowflake and automatically generate a table schema for those listings, reviews, calendar, and communities tables.
+   Once those CSVs were broken down, we used the Snowflake Console Wizard to create tables within a database instance and schema inside of Snowflake and automatically generate a table schema for those listings, reviews, calendar, and communities tables.
 
-    To complete loading, we then loaded any CSV files that were broken into chunks into their corresponding tables, the Snowflake Console Wizard was able to resolve any row and column header conflicts for us.
-    """
+   To complete loading, we then loaded any CSV files that were broken into chunks into their corresponding tables, the Snowflake Console Wizard was able to resolve any row and column header conflicts for us.
+   """
 )
 
 st.image("https://drive.google.com/uc?export=view&id=1hhI7bCGNpDuOr9DdfbnQM9H6iPZYkHN2", caption="Snowflake Data Import")
+
+st.markdown(
+   """
+   # Tools and Technologies
+   """
+)
+
+st.image("https://drive.google.com/uc?export=view&id=1u2aRkC_33ml5rPPzlm3QcN4tY_W_kDA1")
+
+st.markdown(
+   """
+   ## Streamlit
+   Streamlit provides interactive dashboard and visualizations that we used to bring our analysis to life. It allows us to continue to glean insights from our database with filtering even after we did reliminary quantitative analysis. It is also the way we're hosting this website for you to view!
+   """
+)
+
+st.image("https://drive.google.com/uc?export=view&id=1050qyAZk51G-KNxDxtWup3FNyxa4_HB6")
+
+st.markdown(
+   """
+   ## Python / Jupyter Notebooks
+   Python and Jupyter Notebooks allowed us to perform data transformation of very messy and uncleaned data. Moreover, it allowed us to run analysis on the data after we imported the data to Snowflake by using the Snowpark package to query our Snowflake database.
+   """
+)
+
+st.image("https://drive.google.com/uc?export=view&id=1U1wJeN37C6iJFyOlMIkR7FiAhFL9OzUP")
+st.markdown("""
+   ## Snowflake
+   Snowflake enabled us to upload our data to the cloud and query it using SQL. Snowflake is very fast because of its "warehouse" capability (compute) that can scale out and up. Because of the power of a scaled up compute, we were able to load 3 million rows into the database in about 3 minutes.
+
+   Snowflake allowed us to not worry too much about the loading process of our data since the Snowflake Console Wizard has the ability to automatically detect the schema of our tables we uploaded, there was limited manual work for us.
+
+   The only specific requirements of Snowflake was that we needed to have tables with clean dataimported, we chose CSVs. Moreover, the requirement was that CSVs uploaded were limited to a file size of 50MB per upload.
+  """
+)
+
